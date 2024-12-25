597eb8d6e8d1df019f8c829335b3e00e46f3a7ee
Naveen-git-h <naveendugana3@gmail.com>
2024-12-25 12:14:50 +0530

    repo added

--- excelr/src/main/java/com/example/model/Mobiles.java
+++ excelr/src/main/java/com/example/model/Mobiles.java
@@ -5,17 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Products {
+@Data
+public class Mobiles {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,9 +26,4 @@ public class Products {
 	@Column(length = 100)
 	private String description;
 	private Double discount;
-	
-	
-	@ManyToOne
-	@JoinColumn(name = "categories_id" )
-	private Categories categories;
 }
